40e433061304b1df43a22183d08c608311e6ecbf
Jason Bronson <jasonbronson@gmail.com>
2018-09-09 18:59:49 -0400

    Add in electrical Page

---
+++ themes/ljb/layouts/home_hero_electrical.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Homepage Hero Layout
+ */
+?>
+
+
+<div class="hero jumbotron jumbotron-fluid electrical">
+  <div class="container-fluid">
+      <div class="hero-text">
+              <h1>Licensed & Insured Electrician</h1>             
+              <h3>Residential &amp; Commercial Jobs</h3> 
+      </div>
+      <div class="banner-cta">
+                <button class="btn btn-primary btn-lg">
+                  <a href="tel:(518) 796-2516"  onclick="gtag_report_conversion()">Call Or Text Now <i class="fa fa-phone" aria-hidden="true"></i> (518) 796-2516</a>
+                </button>
+      </div>   
+      
+      <div class="hero-text2 container-fluid row pt-4">
+            <div class="col-md-6 col-lg-6 col-sm-6 offer_row">
+                  <div class="row">
+                  <div class="col-md-6 col-sm-6">
+                    <div class="offer mb-4">
+                      <h3>$25 Off</h3>
+                      <p>
+                        Panel Box Installations
+                      </p>
+                      <p class="coupon-box">Call Now & Save</p> 
+                    </div>
+                  </div>
+                  <div class="col-md-6 col-sm-6">
+                    <div class="offer mb-4">
+                      <h3>$25 Off</h3>
+                      <p>
+                        Small Electrical Service
+                      </p>   
+                      <p class="coupon-box">Call Now & Save</p>                 
+                    </div>                  
+                  </div>       
+
+                  </div>         
+            </div>     
+
+            <div class="checklist col-md-6 col-lg-6 col-sm-6 text-center">
+              <div class="form-heading">
+               <span>Why Choose Us?</span>
+              </div>
+              <ul class="list-unstyled bullet-checklist banner-checklist">
+                <li>
+                  <span>Trusted For Over 40 Years</span>
+                </li>
+                <li>
+                  <span>Next Day Service</span>
+                </li>
+                <li>
+                  <span>Reliable Contractor</span>
+                </li>
+                <li>
+                  <span>We Actually Answer Calls</span>
+                </li> 
+                <li>
+                  <span>Old Fashioned Values</span>
+                </li> 
+                
+              </ul>
+            </div>
+       </div>
+  </div>
+</div>
+
---
+++ themes/ljb/page-electrical.php
@@ -0,0 +1,152 @@
+<?php
+/* Template Name: Home Page */
+get_header();
+?>
+<div class="container-fluid">
+
+<?php get_template_part( 'layouts/home_hero_electrical' ); ?>
+
+<div class="section services">
+    <div class="container p-5">
+        <div class="row">
+                <div class="col-md-6 h-100 "> 
+                    <h4 class="title ">Electrical Services</h4>
+                    <div class="row">
+                        <div class="col-md-6"><i class="fas fa-check"> Ceiling Fan Installation & Repair</i></div>
+                        <div class="col-md-6"> <i class="fas fa-check"> Light Switch Installation & Repair</i></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6"><i class="fas fa-check"> Barn Wiring Installation</i></div>
+                        <div class="col-md-6"> <i class="fas fa-check"> Dishwasher Wiring</i></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6"><i class="fas fa-check"> Three way switch Wiring </i></div>
+                        <div class="col-md-6"> <i class="fas fa-check"> Garbage Disposal Wiring </i></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6"><i class="fas fa-check"> New Bathroom Wiring</i></div>
+                        <div class="col-md-6"> <i class="fas fa-check"> Garage Wiring</i></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6"><i class="fas fa-check"> All Electrical </i></div>
+                        <div class="col-md-6"> <i class="fas fa-check"> Hot Tub Wiring </i></div>
+                    </div>
+                 
+                </div>
+                <div class="col-md-6 h-100 "> 
+                    <h4 class="title "> </h4>
+                    <div class="row">
+                            <div class="col-md-6"><i class="fas fa-check"> 200 AMP Panel Installations</i></div>
+                            <div class="col-md-6"> <i class="fas fa-check"> New Wiring </i></div>
+                    </div>
+                    <div class="row">
+                            <div class="col-md-6"><i class="fas fa-check"> Sub Panel Electrical</i></div>
+                            <div class="col-md-6"> <i class="fas fa-check"> Barn Electrical Installations </i></div>
+                    </div>
+                    <div class="row">
+                            <div class="col-md-6"><i class="fas fa-check"> Electrical Commercial Services </i></div>
+                            <div class="col-md-6"> <i class="fas fa-check"> Electrical Residential Services</i></div>
+                    </div>
+                    <div class="row">
+                            <div class="col-md-6"><i class="fas fa-check"> Wiring Problems </i></div>
+                            <div class="col-md-6"> <i class="fas fa-check"> New Wiring Installs</i></div>
+                    </div>
+                    <div class="row">
+                            <div class="col-md-6"><i class="fas fa-check"> Electrical Remodels </i></div>
+                            <div class="col-md-6"> <i class="fas fa-check"> Garage Power Service</i></div>
+                    </div>
+            </div>
+                
+        </div>
+    </div>
+</div>
+
+<div class="section services">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6"><img class="img-fluid" src='<?php echo get_template_directory_uri(); ?>/img/electrical2.jpg'> </div>
+            <div class="col-md-6 services-wording">
+                <h2>Electrical Services</h2>
+                <p>Louis Bronson has been handling electrical services for about 48 years now. His motto is "Do it right or don't do it at all". We work around Glens Falls, NY, Saratoga Springs, NY, Ballston Spa, NY, Lake George, NY and surrounding areas. </p>
+                <p>All our work done for electrical is the highest quality electrical job. We have A+ Ratings with BBB, Angies List, Porch.com. We're also fully insured but have never in 48 years needed to use it. We stand behind our work and will make sure you're happy with the job done.</p>
+                <br>
+                <h1></h1>
+                <p></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="section services bk-blue align-middle">
+    <div class="container text-center">
+        <h2>Electrical Services Glens Falls, Saratoga Springs, Lake George</h2>
+        <h5>Affordable, Dependable, Professional, Old School Values, Amazing are what comes to mind from our customers. </h5>
+        <p>Make sure to get a Free Quote from us. We love our customers and they love us. It's a long term relationship not a one time call.</p>
+        <button class="btn btn-primary btn-lg">
+                  <a href="tel:(518) 796-2516"  onclick="gtag_report_conversion()">Call Or Text Now <i class="fa fa-phone" aria-hidden="true"></i> (518) 796-2516</a>
+                </button>
+    </div>
+</div>
+
+<div class="section services">
+    <div class="container p-5">
+        <div class="row">
+                <div class="col-md-6 h-100 "> 
+                    <h4 class="title ">What Can We Service</h4>
+                    <div class="row">
+                        <div class="col-md-6"><i class="fas fa-check"> Electrical Inspections</i></div>
+                        <div class="col-md-6"> <i class="fas fa-check"> Breakers and Fuses</i></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6"><i class="fas fa-check"> Service Panel Upgrades</i></div>
+                        <div class="col-md-6"> <i class="fas fa-check"> Move Light Fixtures</i></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6"><i class="fas fa-check"> Recessed Lighting </i></div>
+                        <div class="col-md-6"> <i class="fas fa-check"> Smoke Detector Wiring</i></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6"><i class="fas fa-check"> Whole Home Surge Protectors</i></div>
+                        <div class="col-md-6"> <i class="fas fa-check"> Ground Fault Circuits</i></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6"><i class="fas fa-check">Dryer Electrical Wiring </i></div>
+                        <div class="col-md-6"> <i class="fas fa-check"> Ballast Replacement</i></div>
+                    </div>
+                 
+            </div>
+            <div class="col-md-6 h-100 "> 
+                <h4 class="title "> &nbsp; </h4>
+                <div class="row">
+                        <div class="col-md-6"><i class="fas fa-check"> Receptacle Repair</i></div>
+                        <div class="col-md-6"> <i class="fas fa-check"> Flickering Light Issues </i></div>
+                </div>
+                <div class="row">
+                        <div class="col-md-6"><i class="fas fa-check"> Sub Panel Electrical</i></div>
+                        <div class="col-md-6"> <i class="fas fa-check"> Barn Electrical Installations </i></div>
+                </div>
+                <div class="row">
+                        <div class="col-md-6"><i class="fas fa-check"> Electrical Commercial Services </i></div>
+                        <div class="col-md-6"> <i class="fas fa-check"> Electrical Residential Services</i></div>
+                </div>
+                <div class="row">
+                        <div class="col-md-6"><i class="fas fa-check"> Wiring Problems </i></div>
+                        <div class="col-md-6"> <i class="fas fa-check"> New Wiring Installs</i></div>
+                </div>
+                <div class="row">
+                        <div class="col-md-6"><i class="fas fa-check"> Electrical Remodels </i></div>
+                        <div class="col-md-6"> <i class="fas fa-check"> Garage Power Service</i></div>
+                </div>
+                
+            
+            </div>
+                
+        </div>
+    </div>
+</div>
+
+
+
+</div>
+<?php get_footer(); ?>
